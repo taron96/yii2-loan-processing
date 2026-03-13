0426608d0d57515cb1255c5ea41c359dcea48add
taron96 <antonyantaron1996@gmail.com>
2026-03-13 13:28:44 +0400

    Add db config file

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class'    => 'yii\db\Connection',
-    'dsn'      => 'pgsql:host=db;port=5432;dbname=loans',
+    'dsn'      => 'pgsql:host=db;port=5432;dbname=loans', // Внутри Docker используем имя сервиса 'db'
     'username' => 'user',
     'password' => 'password',
     'charset'  => 'utf8',
